90a23e06845844175a1a728d7315f8df03db2ab5
octantast <persimorrel@gmail.com>
2024-01-21 09:47:42 +0200

    Init

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -19,11 +19,11 @@ public class Button : MonoBehaviour//, IBeginDragHandler, IDragHandler, IEndDrag
     private void FixedUpdate()
     {
         transform.localScale = new Vector3(startSize, startSize, startSize); // Vector3.Lerp(transform.localScale, new Vector3(startSize, startSize, startSize), 10 * Time.deltaTime);
-        thisImage.color = Color.Lerp(thisImage.color, startColor, 10 * Time.deltaTime);
+       // thisImage.color = startColor; //Color.Lerp(thisImage.color, startColor, 10 * Time.deltaTime);
     }
     public void OnDown()
     {
-        thisImage.color = new Color32(255, 255, 255, 130);
+       // thisImage.color = new Color32(255, 255, 255, 130);
         transform.localScale = new Vector3(startSize + 0.1f, startSize + 0.1f, startSize + 0.1f);
     }
 
